ca379280ac6e9482c4fcd694606e79e62ab77fd2
HeckRodSav <hrs.heitor@hotmail.com.br>
2019-05-25 18:53:40 -0300

    0.4.0 - Big update
    - The code has been rearranged to group similar commands
    - Cobra leaves a trail wherever he goes
    - The snake's head is different from the body
    - Now the board shows an edge (the snake crosses the other side using the magical power of the snakes)
    - The first line shows:
    - - The last direction chosen
    - - The size of the snake
    - - The coordinate of the snake head
    - The look of any game item can be easily changed with a preprocessor directive

--- snaker.cpp
+++ snaker.cpp
@@ -91,6 +91,16 @@ void clearScreen()
 #define ALTURA 30
 #define LARGURA 30
 #define DELAY 0.1
+#define TRAIL_SIZE ((ALTURA+LARGURA)/4)
+
+#define AUTOWALK 1
+#define AUTOGROW 1
+
+#define WALL_CHAR  '#'
+#define HEAD_CHAR  '@'
+#define BODY_CHAR  'S'
+#define TRAIL_CHAR '.'
+#define EMPTY_CHAR ' '
 
 using namespace std;
 
@@ -104,19 +114,24 @@ struct coord
     int x, y;
 };
 
+static const char commands[] = {'w','a','s','d'};
+
 int main()
 {
-    list<coord> body;
-
+    list<coord> body, trail;
     char input('\0');
     bool fim(false);
+
 	int size(1), t_counter(0);
+    bool grow(false);
     int I(ALTURA/2), J(LARGURA/2);
     body.push_front(coord(I,J));
-    bool grow(false);
+
 
     char** table = new char*[ALTURA]; // {}
     for(int i=0; i<ALTURA; i++) table[i] = new char[LARGURA];
+
+    for(int i=0; i<ALTURA; i++) memset(table[i],EMPTY_CHAR,sizeof(char)*LARGURA); //Limpa tabuleiro
 
     double Tnow(double(clock())/CLOCKS_PER_SEC);
     double Tbefore(Tnow);
@@ -125,19 +140,37 @@ int main()
 
     while(!fim)
 	{
-        while(kbhit()==0 && Tnow-Tbefore<DELAY) Tnow = double(clock())/CLOCKS_PER_SEC;
-        if(kbhit()) input = getch();
-        Tbefore = Tnow;
-
-        if(((++t_counter)%=100)==0)
+#if AUTOGROW
+        if(((++t_counter)%100)==0)
         {
             size++;
             grow = true;
         }
+#endif
 
-		clearScreen();
+#if AUTOWALK
+        if(t_counter%1==0)
+        {
+            char R('\0');
+            bool r(false);
+            do
+            {
+                r = false;
+                R = commands[rand()%3+1];
+                r |= (R=='a' && input=='d');
+                r |= (R=='d' && input=='a');
+                r |= (R=='w' && input=='s');
+                r |= (R=='s' && input=='w');
+            }
+            while(r);
+            input = R;
+        }
+#endif
 
-		cout << input << endl;
+        while(kbhit()==0 && Tnow-Tbefore<DELAY) Tnow = double(clock())/CLOCKS_PER_SEC;
+        if(kbhit()) input = getch();
+        Tbefore = Tnow;
+
         switch(input)
         {
             case 'w':
@@ -169,26 +202,41 @@ int main()
 
         body.push_front(coord(I%ALTURA,J%LARGURA));
         if(grow) grow = false;
-        else body.pop_back();
+        else
+        {
+            coord C = body.back();
+            trail.push_front(C);
+            table[C.x][C.y] = TRAIL_CHAR;
+            body.pop_back();
+        }
+        if(trail.size()>TRAIL_SIZE)
+        {
+            coord C = trail.back();
+            trail.pop_back();
+            table[C.x][C.y]=EMPTY_CHAR;
+        }
 
-        for(int i=0; i<ALTURA; i++) for(int j=0; j<LARGURA; j++) table[i][j]='.'; //Limpa tabuleiro
+        // for(int i=0; i<ALTURA; i++) for(int j=0; j<LARGURA; j++) table[i][j]='.'; //Limpa tabuleiro
 
         //table[I%ALTURA][J%LARGURA] = '#';
-        for(list<coord>::iterator dot=body.begin(); dot != body.end(); dot++)
-        {
-            coord C = *dot;
-            //cout << "(" << C.x << "," << C.y << ")" << endl;
-            table[C.x][C.y]='#';
-        }
+        for(list<coord>::iterator dot=body.begin(); dot != body.end(); dot++) table[dot->x][dot->y]=BODY_CHAR;
+        table[body.front().x][body.front().y] = HEAD_CHAR;
 
+		clearScreen();
+
+        cout << input << "; " << size << " (" << I%ALTURA << "," << J%LARGURA << ")" << endl;
+
+        for(int j = 0; j < LARGURA + 2; j++) cout << WALL_CHAR << " "; cout << endl;
         for(int i = 0; i < ALTURA; i++)
         {
+            cout << WALL_CHAR << " " ;
             for(int j = 0; j < LARGURA; j++)
             {
-                cout << table[i][j] << ' ';
+                cout << table[i][j] << " ";
             }
-            cout << endl;
+            cout << WALL_CHAR << endl;
         }
+        for(int j = 0; j < LARGURA + 2; j++) cout << WALL_CHAR << " "; cout << endl;
     }
 
     for (int i=0; i<ALTURA; i++) delete[] table[i];
