43f9e5227a6e2753c4d72c422a8045b1f329ec9c
HeckRodSav <hrs.heitor@hotmail.com.br>
2019-05-25 14:26:19 -0300

    0.1.0 - Player interaction
    - WASD keys move snake
    - Q key closes the game

--- snaker.cpp
+++ snaker.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 #if defined(_WIN32)
 
@@ -10,7 +11,7 @@
 
 #include <unistd.h>
 #include <sys/select.h>
-#include <	.h>
+#include <termios.h>
 
 struct termios tty_settings;
 
@@ -76,6 +77,15 @@ void enable_getch()  //Configura terminal especial // set_conio_terminal_mode
 #endif
 }
 
+void clearScreen()
+{
+#if defined(__linux__) || defined(__APPLE__)
+    if (tty_available()) {
+        fputs("\x1b[2J\x1b[1;1H", stdout);
+    }
+#endif
+}
+
 #define ALTURA 10
 #define LARGURA 5
 
@@ -83,23 +93,57 @@ using namespace std;
 
 int main()
 {
+    char input('\0');
+    bool fim(false);
     int I(ALTURA/2), J(LARGURA/2);
+		enable_getch();
 
-    enable_getch();
+    while(!fim)
+	{
+		while(kbhit()==0);
+        input = getch();
 
-    disable_getch();
+		clearScreen();
 
-    //int V[ALTURA][LARGURA];
-    I%=ALTURA;
-    J%=LARGURA;
-    for(int i = 0; i < ALTURA; i++)
-    {
-        for(int j = 0; j < LARGURA; j++)
+		cout << input << endl;
+        switch(input)
         {
-            if ( i==I && j==J ) cout << "# ";
-            else cout << ". ";
+            case 'w':
+            case 'W':
+                I--;    //Sobe
+                break;
+            case 's':
+            case 'S':
+                I++;    //Desce
+                break;
+            case 'a':
+            case 'A':
+                J--;
+                break;
+            case 'd':
+            case 'D':
+                J++;
+                break;
+			case 'q':
+			case 'Q':
+                fim = true;
+                break;
+            default:;
         }
-        cout << endl;
+
+		//int V[ALTURA][LARGURA];
+		I%=ALTURA;
+		J%=LARGURA;
+		for(int i = 0; i < ALTURA; i++)
+		{
+			for(int j = 0; j < LARGURA; j++)
+			{
+				if ( i==I && j==J ) cout << "# ";
+				else cout << ". ";
+			}
+			cout << endl;
+		}
     }
-    
+		disable_getch();
+	return 0;
 }
